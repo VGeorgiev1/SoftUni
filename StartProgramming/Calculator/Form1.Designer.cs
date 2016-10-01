a1388cd1dc5e06b28e6da28fd6defafa6a914c92
Vladislav <vladdislav.georgiev@gmail.com>
2016-10-01 14:39:05 +0300

    Start

---
+++ StartProgramming/Calculator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.First = new System.Windows.Forms.TextBox();
+            this.Second = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // First
+            // 
+            this.First.Location = new System.Drawing.Point(12, 113);
+            this.First.Name = "First";
+            this.First.Size = new System.Drawing.Size(63, 20);
+            this.First.TabIndex = 1;
+            this.First.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Second
+            // 
+            this.Second.Location = new System.Drawing.Point(101, 113);
+            this.Second.Name = "Second";
+            this.Second.Size = new System.Drawing.Size(63, 20);
+            this.Second.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "=";
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(189, 113);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(62, 20);
+            this.result.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Second);
+            this.Controls.Add(this.First);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox First;
+        private System.Windows.Forms.TextBox Second;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
