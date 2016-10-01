142fd1c4136e07586cc6fca07ac8858d8528ef29
Vladislav <vladdislav.georgiev@gmail.com>
2016-10-01 15:20:04 +0300

    addition

---
+++ Solution1/LatinLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstletter = 'a';
+            char lastletter = 'z';
+            for(firstletter='a'; firstletter <= lastletter; firstletter++)
+            {
+                Console.WriteLine(firstletter);
+
+            }
+        }
+    }
+}
---
+++ Solution1/Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i;
+            int numb;
+            int sum = 0;
+            for (i = 0; i < n; i++)
+            {
+                numb = int.Parse(Console.ReadLine());
+                sum = sum + numb;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
